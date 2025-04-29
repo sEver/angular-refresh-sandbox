58595be661186069c2124631ee9338cd3e4d2567
sEver <sEver@users.noreply.github.com>
2025-04-30 01:15:05 +0200

    Add route handling in details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 @Component({
   selector: 'app-details',
   imports: [],
@@ -7,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId: number = -1;
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
