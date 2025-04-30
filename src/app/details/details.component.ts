4cd29d5dad1e92d2f7a80616aa405ee9531f6ca7
sEver <sEver@users.noreply.github.com>
2025-04-30 02:18:29 +0200

    Add basic form and handling

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
@@ -13,10 +14,21 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
-
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
